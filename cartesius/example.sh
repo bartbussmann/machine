0dc49f8ee2daecb91183f6af8d3231ab239e4e64
Bart <bart@bit-students.com>
2018-01-24 15:42:15 +0100

    new try

--- cartesius/example.sh
+++ cartesius/example.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
-cd /home/ebruni/metaphora-detection/OpenNMT
-python train.py -data ../new-data/demo-train.pt -save_model model -cuda -rnn_size 200 -word_vec_size 200
-echo Done
+#! /bin/sh
 
+TRAIN_PATH=test/test_data/train.txt
+DEV_PATH=test/test_data/dev.txt
+EXPT_DIR=example
+
+# set values
+EMB_SIZE=128
+H_SIZE=128
+CELL='gru'
+EPOCH=6
+PRINT_EVERY=10
+TF=0.5
+
+# Start training
+echo "Train model on example data"
+python train_model.py --train $TRAIN_PATH --dev $DEV_PATH --output_dir $EXPT_DIR --print_every 50 --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --epoch $EPOCH --print_every $PRINT_EVERY --teacher_forcing $TF --bidirectional --attention
+
+echo "Evaluate model on test data"
+python evaluate.py --checkpoint_path $EXPT_DIR/$(ls -t $EXPT_DIR/ | head -1) --test_data $DEV_PATH
+
+echo "Run in inference mode"
+python infer.py --checkpoint_path $EXPT_DIR/$(ls -t $EXPT_DIR/ | head -1) 
