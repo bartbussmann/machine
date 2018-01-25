5507709310cf92e2d378f310ed11a644d5832e1e
Bart <bart@bit-students.com>
2018-01-25 12:11:23 +0100

    new experiment

--- example.sh
+++ example.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-TRAIN_PATH=data/CLEANED-SCAN/length_split/experiment4a_output_short_to_long_equally_distributed/output.txt
-DEV_PATH=data/CLEANED-SCAN/length_split/experiment4a_output_short_to_long_equally_distributed/tasks_test.txt
-EXPT_DIR=checkpoints_experiment_4a
+TRAIN_PATH=data/CLEANED-SCAN/length_split/experiment4b_output_long_to_short_equally_distributed/output.txt
+DEV_PATH=data/CLEANED-SCAN/length_split/experiment4b_output_long_to_short_equally_distributed/tasks_test.txt
+EXPT_DIR=checkpoints_experiment_4b
 ATTENTION=true
 CUDA=0
 
