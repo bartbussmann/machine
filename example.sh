f565442a625d155c60b7e9cf54d014bb97343048
Bart <bart@bit-students.com>
2018-02-05 17:43:59 +0100

    experiment 5_5

--- example.sh
+++ example.sh
@@ -2,7 +2,7 @@
 
 TRAIN_PATH=data/CLEANED-SCAN/length_split/experiment5/train.txt
 DEV_PATH=data/CLEANED-SCAN/length_split/experiment5/dev.txt
-EXPT_DIR=checkpoints_experiment_5_4
+EXPT_DIR=checkpoints_experiment_5_5
 ATTENTION=true
 CUDA=0
 
